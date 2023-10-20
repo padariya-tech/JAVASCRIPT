b403f52a82a92c2df8969317679a2e9b072b6957
nij <nijpadariya@gmail.com>
2023-10-20 14:40:05 +0530

    updated

---
+++ var_const.js
@@ -0,0 +1,15 @@
+const accountId=10
+let accountEmail = "nijp   a@gmail.com"
+var accountpassword = "123321"
+accountcity = "rajkot"
+
+console.table([accountEmail,accountId,accountpassword]);
+
+ accountEmail = "nijpadariya@gmail.com"
+accountpassword = "12332232321"
+accountcity = "gondal"
+let accountState;
+
+console.table([accountState,accountEmail,accountId,accountpassword]);
+
+// prefer not use var because of issue in block scope and functional scope
