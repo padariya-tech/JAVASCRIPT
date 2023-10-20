baf44afcbfccf6c2f78f2c693c46829db4dd69a3
nij <nijpadariya@gmail.com>
2023-10-20 15:09:23 +0530

    changed

---
+++ data_type2.js
@@ -0,0 +1,14 @@
+let score ="33wewrwr"
+// console.log(typeof score);
+
+let valueinscore = Number(score);
+// console.log(valueinscore); // NaN
+// console.log(typeof valueinscore); // number
+
+// "33"=33
+// "33abc"=NaN
+// true =1,false=0
+
+let isloggedin = ""
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin);
