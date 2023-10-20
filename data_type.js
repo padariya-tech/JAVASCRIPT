74abc8e0d58717508ddaaf70266a170358a87734
nij <nijpadariya@gmail.com>
2023-10-20 14:58:00 +0530

    data_types

---
+++ data_type.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) we are using nodejs , not browser
+
+let name = "nij"
+let age = 21
+let ans = true
+if (age>22) {
+    console.log(ans);
+}
+console.log(!ans);
+
+// number = 2 to power 53
+// BigInt
+// String = ""
+// boolean = true/false
+// null => standalone value
+// undefined
+// symbol => unique
+// object 
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
