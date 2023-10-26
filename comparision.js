9029402d6424027b3f4050349aa15d01563f6e4f
padariya-tech <nijpadariya@gmail.com>
2023-10-26 18:41:34 +0530

    first commit

---
+++ comparision.js
@@ -0,0 +1,18 @@
+// console.log(2>1);
+// console.log("2">1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// the reason is that an equality check == and comparision > < >= <= work differently
+
+// comprision convert null to a number, treating it as 0. that's why (3) nll>=0 is true ans null > 0 is false</>
+
+console.log( undefined> 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+console.log("2"===2);
+
+console.log(2===2);
---
+++ data_type3.js
@@ -0,0 +1,9 @@
+//  Primitive
+
+//  7 types : String , number , Boolean , null , undefined , Symbol , BigInt
+
+  
+
+//  reference (non primitive)
+
+//  Array,objects , functions
