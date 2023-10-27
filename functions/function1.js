946361205017c912782e98805f72c5947e924dc8
padariya-tech <nijpadariya@gmail.com>
2023-10-27 21:55:41 +0530

    functions intro

---
+++ functions/function1.js
@@ -0,0 +1,21 @@
+function calculateCartprice(val1,al2,val3,...num1){
+    return num1;
+}
+
+// console.log(calculateCartprice(2,2,3,4,5,6,7));
+
+const user={
+    username:"hitesh",
+    price:199
+}
+
+function handleObject(anyobject)
+{
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username:"sam",
+    price:199
+})
